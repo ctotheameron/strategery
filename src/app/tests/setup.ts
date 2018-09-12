c8bf4990e24ab150aea81108662e1864349acedb
ctotheameron <cameron@pro.com>
2018-09-11 17:54:03 -0700

    Adding global.CONFIG hack for jest tests

--- src/app/tests/setup.ts
+++ src/app/tests/setup.ts
@@ -1,4 +1,14 @@
+import { resolve } from 'path';
+
+process.env.NODE_CONFIG_DIR = resolve(__dirname, '../config');
+process.env.SUPPRESS_NO_CONFIG_WARNING = 'true';
+process.env.NODE_CONFIG_STRICT_MODE = 'true';
+
+import * as config from 'config';
 import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
+
+const globalAny: any = global;
+globalAny.CONFIG = config;
 
 configure({ adapter: new Adapter() });
